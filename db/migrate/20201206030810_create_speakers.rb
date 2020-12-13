3cfd6520f3498e718843fc558186327aa9142932
r_takaishi <ryo.takaishi.0@gmail.com>
2020-12-13 16:08:23 +0900

    profileじゃなくてspeakerだった

--- db/migrate/20201206030810_create_speakers.rb
+++ db/migrate/20201206030810_create_speakers.rb
@@ -1,8 +1,13 @@
-class CreateProfiles < ActiveRecord::Migration[6.0]
+class CreateSpeakers < ActiveRecord::Migration[6.0]
   def change
-    create_table :profiles do |t|
-      t.string :first_name
-      t.string :last_name
+    create_table :speakers do |t|
+      t.string :name
+      t.string :sub
+      t.string :profile
+      t.string :company
+      t.string :job_title
+      t.string :twitter_id
+      t.string :github_id
       t.string :email
 
       t.timestamps
