6baf3607719277e97104647061429e14d10a5976
r_takaishi <ryo.takaishi.0@gmail.com>
2020-12-16 21:00:27 +0900

    add some conference to seeds

--- db/fixtures/development/seeds.rb
+++ db/fixtures/development/seeds.rb
@@ -1,9 +1,22 @@
 Conference.seed(
   {
     id: 1,
+    name: "Test Event Autumn 2020",
+    abbr: "tea2020",
+    status: 2, # closed
+  },
+  {
+    id: 2,
     name: "Test Event Winter 2020",
-    abbr: "tew2020"
-  }
+    abbr: "tew2020",
+    status: 1, # opened
+  },
+  {
+    id: 3,
+    name: "Test Event Summer 2021",
+    abbr: "tes2021",
+    status: 0, # registered
+  },
 )
 
 Speaker.seed(
